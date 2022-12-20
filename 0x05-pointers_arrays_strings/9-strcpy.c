07b37d124d4d2201e0cfd821dfb33c3af0098311
thresa <thresagimbo@gmail.com>
2022-12-19 17:20:47 -0800

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -19,27 +19,14 @@
  *        */
 
 char *_strcpy(char *dest, char *src)
-
 {
-
-		int count = 0;
-
-
-
-			while (count >= 0)
-
-					{
-
-								*(dest + count) = *(src + count);
-
-										if (*(src + count) == '\0')
-
-														break;
-
-												count++;
-
-													}
-
-				return (dest);
-
+	int count = 0;
+		while (count >= 0)
+{
+*(dest + count) = *(src + count);
+if (*(src + count) == '\0')
+reak;
+count++;
+}
+return (dest);
 }
