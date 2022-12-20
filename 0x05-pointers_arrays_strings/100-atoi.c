5eea8b18183e74469f0158ab1afbefd3d1fe0161
thresa <thresagimbo@gmail.com>
2022-12-19 16:50:37 -0800

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
  *
  *    * Return: integer.
  *
- *     */
+ */
 
 int _atoi(char *s)
 {
@@ -27,10 +27,9 @@ while (*(s + count) != '\0')
 	count++;
 }
 for (i = count - size; i < count; i++)
-
-	{
-		oi = oi + ((*(s + i) - 48) * m);
-				m /= 10;
-	}
+ {
+    oi = oi + ((*(s + i) - 48) * m);
+    m /= 10;
+ }
 return (oi * pn);
 }
