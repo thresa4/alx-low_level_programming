16873e906d0acd50d47f0d5510761e2a322718be
thresa <thresagimbo@gmail.com>
2022-12-20 13:31:24 -0800

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,27 +3,14 @@
 
 
 /**
- *
- *  * _strlen - returns the length of a string.
- *
- *   * @s: input stringeturn.
- *
- *    * Return: length of a string.
- *
- *     */
-
+ * _strlen - returns the length of a string.
+ * @s: input stringeturn.
+ * Return: length of a string.
+ */
 int _strlen(char *s)
-
 {
-
-		int count = 0;
-
-
-
-			while (*(s + count) != '\0')
-
-						count++;
-
-				return (count);
-
+	int count = 0;
+		while (*(s + count) != '\0')
+				count++;
+			return (count);
 }
